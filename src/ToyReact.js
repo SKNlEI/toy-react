e4d543002519ba6d56ade2210d916c70408bae39
DESKTOP-46BFAVA\skl <1907513952@qq.com>
2020-08-15 12:35:44 +0800

    fix

--- src/ToyReact.js
+++ src/ToyReact.js
@@ -1,3 +1,36 @@
+class ElementWrapper {
+  constructor(type) {
+    this.root = document.createElement(type)
+  }
+
+  setAttribute(name, value) {
+    this.root.setAttribute(name, value)
+  }
+
+  appendChild(vchild) {
+    vchild.mountTo(this.root)
+  }
+
+  mountTo(parent) {
+    parent.appendChild(this.root)
+  }
+}
+
+class TextWrapper {
+  constructor(type) {
+    this.root = document.createElement(type)
+  }
+
+  setAttribute(name, value) {
+    this.root.setAttribute(name, value)
+  }
+
+  appendChild(vchild) {
+    vchild.mountTo(this.root)
+  }
+}
+
+
 export let ToyReact = {
   createElement (type, attributes, ...children) {
     let element = document.createElement(type)
@@ -11,5 +44,9 @@ export let ToyReact = {
       element.appendChild(child)
     }
     return element
+  },
+
+  render(vdom, element) {
+    vdom.mountTo(element)
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import { ToyReact } from './ToyReact'
 
+class MyComponent {
+  render(h) {
+    return <div>render-test</div>
+  }
 
-let a = <div name="test">
+  mountTo(parent) {
+    let dom = this.render()
+    vdom.mountTo(parent)
+  }
+}
+
+
+let a = <MyComponent name="test">
   <span>hello</span>
   <span>world</span>
   <span></span>
-</div>
+</MyComponent>
+
+ToyReact.render(
+  a,
+  document.body
+)
 
 
 console.log(a)
 document.body.appendChild(a)
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _ToyReact__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ToyReact */ "./src/ToyReact.js");
+
+var a = _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement(
+  "div", 
+  { name: "test" },
+  _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("span", null, "hello"),
+  _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("span", null, "world"), 
+  _ToyReact__WEBPACK_IMPORTED_MODULE_0__["ToyReact"].createElement("span", null)
+);
+console.log(a);
+document.body.appendChild(a);
